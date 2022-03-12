6ec485a214ebe39326455d0c165ec4d90cbb5cc2
Haziq Hapiz <zyqq.dev@gmail.com>
2022-03-12 22:26:33 +0800

    update countdown

--- src/components/Countdown.js
+++ src/components/Countdown.js
@@ -1,29 +1,44 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 
 const Countdown = () => {
-    // set time interval to countdown from todays date until 10 April 2022
-    const [count, setCount] = useState(new Date("April 10, 2022").getTime());
 
-    const [days, setDays] = useState(0);
-    const [hours, setHours] = useState(0);
-    const [minutes, setMinutes] = useState(0);
-    const [seconds, setSeconds] = useState(0);
-    
-    useEffect(() => {
-        const interval = setInterval(() => {
-            setCount(count - 1000);
+    const [days, setDays] = useState('00');
+    const [hours, setHours] = useState('00');
+    const [minutes, setMinutes] = useState('00');
+    const [seconds, setSeconds] = useState('00');
+
+    let interval = useRef();
+
+    const startTimer = () => {
+        const countdownDate = new Date('April 2, 2022 19:30:00').getTime();
+
+        interval = setInterval(() => {
+            const now = new Date().getTime();
+            const distance = countdownDate - now;
+
+            const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+            const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+            const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+            if (distance < 0) {
+                clearInterval(interval);
+                return;
+            }
+            else {
+                setDays(days);
+                setHours(hours);
+                setMinutes(minutes);
+                setSeconds(seconds);
+            }
         }, 1000);
-        return () => clearInterval(interval);
-    }, [count]);
+    }
 
     useEffect(() => {
-        const timeLeft = new Date(count);
-        setDays(timeLeft.getUTCDate() - new Date().getUTCDate());
-        setHours(timeLeft.getUTCHours() - new Date().getUTCHours());
-        setMinutes(timeLeft.getUTCMinutes() - new Date().getUTCMinutes());
-        setSeconds(timeLeft.getUTCSeconds() - new Date().getUTCSeconds());
-    }, [count]);
-
+        startTimer();
+        return () => clearInterval(interval);
+    });
+    
     return (
         <div className="countdown">
             <div className="countdown__item">
@@ -45,47 +60,5 @@ const Countdown = () => {
         </div>
     );
 };
-    
-//     // const [count, setCount] = useState(5);
-    
-//     useEffect(() => {
-//         if (count > 0) {
-//         const interval = setInterval(() => {
-//             setCount(count - 1);
-//         }, 1000);
-    
-//         return () => clearInterval(interval);
-//         }
-//     }, [count]);
-    
-//     // convert count to display in date format
-//     const days = Math.floor(count / (1000 * 60 * 60 * 24));
-//     const hours = Math.floor((count % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-//     const minutes = Math.floor((count % (1000 * 60 * 60)) / (1000 * 60));
-//     const seconds = Math.floor((count % (1000 * 60)) / 1000);
-
-
-//     return (
-//         <div className="countdown">
-//             <div className="countdown-item">
-//                 <div className="countdown-item-label">Days</div>
-//                 <div className="countdown-item-amount">{days}</div>
-//             </div>
-//             <div className="countdown-item">
-//                 <div className="countdown-item-label">Hours</div>
-//                 <div className="countdown-item-amount">{hours}</div>
-//             </div>
-//             <div className="countdown-item">
-//                 <div className="countdown-item-label">Minutes</div>
-//                 <div className="countdown-item-amount">{minutes}</div>
-//             </div>
-//             <div className="countdown-item">
-//                 <div className="countdown-item-label">Seconds</div>
-//                 <div className="countdown-item-amount">{seconds}</div>
-//             </div>
-//         <h1>{count}</h1>
-//         </div>
-//     );
-// };
 
 export default Countdown;
